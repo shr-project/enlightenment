f6dcaa9cb785f2244423976a23895649b9a251be
stefan <stefan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-09-26 10:48:20 +0000

    PROTO/elocation: Add library skeleton for efl location.
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@77079 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

---
+++ PROTO/elocation/src/bin/geoclue-test.c
@@ -0,0 +1,183 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+
+#include <Ecore.h>
+#include <E_DBus.h>
+#include <Elocation.h>
+
+/* TODO:
+   o Create a client object to operate on
+   o Ask master for providers
+   o Set requirements
+   o Attach to signals and give out ecore events
+   o Start (how to stop?) service to get position updates
+   o Look for nameOwner changed to issue IN and OUT for geoclue
+*/
+
+static E_DBus_Signal_Handler *cb_position_changed = NULL;
+
+void
+create_cb(void *data , DBusMessage *reply, DBusError *error)
+{
+   const char *object_path;
+   DBusMessageIter iter;
+
+   if (!dbus_message_has_signature(reply, "o")) return;
+
+   dbus_message_iter_init(reply, &iter);
+   dbus_message_iter_get_basic(&iter, &object_path);
+
+   printf("Object path for client: %s\n", object_path);
+}
+
+void
+status_cb(void *data , DBusMessage *reply, DBusError *error)
+{
+   dbus_int32_t status;
+   DBusMessageIter iter;
+
+   if (!dbus_message_has_signature(reply, "i")) return;
+
+   dbus_message_iter_init(reply, &iter);
+   dbus_message_iter_get_basic(&iter, &status);
+
+   printf("Status: %i\n", status);
+}
+
+void
+position_cb(void *data , DBusMessage *reply, DBusError *error)
+{
+   GeocluePositionFields fields;
+   dbus_int32_t level, timestamp;
+   double horizontal = 0.0;
+   double vertical = 0.0;
+   double latitude = 0.0;
+   double longitude = 0.0;
+   double altitude = 0.0;
+   DBusMessageIter iter, sub;
+   gc_accuracy accur;
+
+   if (dbus_error_is_set(error))
+     {
+      printf("Error: %s - %s\n", error->name, error->message);
+      return;
+     }
+
+   if (!dbus_message_has_signature(reply, "iiddd(idd)")) return;
+
+   dbus_message_iter_init(reply, &iter);
+
+   dbus_message_iter_get_basic(&iter, &fields);
+   dbus_message_iter_next(&iter);
+
+   dbus_message_iter_get_basic(&iter, &timestamp);
+   dbus_message_iter_next(&iter);
+
+   dbus_message_iter_get_basic(&iter, &latitude);
+   dbus_message_iter_next(&iter);
+
+   dbus_message_iter_get_basic(&iter, &longitude);
+   dbus_message_iter_next(&iter);
+
+   dbus_message_iter_get_basic(&iter, &altitude);
+   dbus_message_iter_next(&iter);
+
+   dbus_message_iter_recurse(&iter, &sub);
+   dbus_message_iter_get_basic(&sub, &level);
+   accur.level = level;
+   dbus_message_iter_next(&sub);
+
+   dbus_message_iter_get_basic(&sub, &horizontal);
+   accur.horizontal = horizontal;
+   dbus_message_iter_next(&sub);
+
+   dbus_message_iter_get_basic(&sub, &vertical);
+   accur.vertical = vertical;
+   dbus_message_iter_next(&sub);
+
+   printf("DBus GeoClue reply with data from timestamp %i\n", timestamp);
+
+   if (fields & GEOCLUE_POSITION_FIELDS_LATITUDE)
+      printf("Latitude:\t %f (valid)\n", latitude);
+   else
+      printf("Latitude:\tinvalid.\n");
+
+   if (fields & GEOCLUE_POSITION_FIELDS_LONGITUDE)
+      printf("Longitude:\t %f (valid)\n", longitude);
+   else
+      printf("Longitude:\tinvalid.\n");
+
+   if (fields & GEOCLUE_POSITION_FIELDS_ALTITUDE)
+      printf("Altitude:\t %f (valid)\n", altitude);
+   else
+      printf("Altitude:\tinvalid.\n");
+
+   printf("Accuracy: level %i, horizontal %f and vertical %f\n", level, horizontal, vertical);
+
+//   ecore_main_loop_quit();
+}
+
+void
+position_signal_cb(void *data , DBusMessage *reply)
+{
+   position_cb(data, reply, NULL);
+}
+
+int
+main()
+{
+   E_DBus_Connection *conn;
+   DBusMessage *msg, *msg2, *msg3;
+   DBusMessageIter iter, sub;
+   int ret = 0;
+   struct gc_accuracy *accur;
+
+   e_dbus_init();
+
+   conn = e_dbus_bus_get(DBUS_BUS_SESSION);
+   if (!conn)
+     {
+      printf("Error: could not connect to session bus.\n");
+      ret = 1;
+     }
+
+   msg3 = dbus_message_new_method_call(GEOCLUE_DBUS_NAME, GEOCLUE_OBJECT_PATH, GEOCLUE_DBUS_NAME, "Create");
+   e_dbus_message_send(conn, msg3, create_cb, -1, NULL);
+   dbus_message_unref(msg3);
+
+#if 0
+   dbus_message_append_args(msg, DBUS_TYPE_INT32, &field, DBUS_TYPE_INT32, &timestamp,
+                                DBUS_TYPE_DOUBLE, &latitude, DBUS_TYPE_DOUBLE, &longitude,
+                                DBUS_TYPE_DOUBLE, &altitude, DBUS_TYPE_INVALID);
+
+   dbus_message_iter_init_append(msg, &iter);
+   if (dbus_message_iter_open_container(&iter, DBUS_TYPE_STRUCT, NULL, &sub))
+     {
+      dbus_message_iter_append_basic(&sub, DBUS_TYPE_INT32, &level);
+      dbus_message_iter_append_basic(&sub, DBUS_TYPE_DOUBLE, &horizontal);
+      dbus_message_iter_append_basic(&sub, DBUS_TYPE_DOUBLE, &vertical);
+     }
+   else
+      printf("dbus_message_iter_open_container() failed\n");
+
+   dbus_message_iter_close_container(&iter, &sub);
+#endif
+   msg = dbus_message_new_method_call(UBUNTU_DBUS_NAME, UBUNTU_OBJECT_PATH, GEOCLUE_POSITION_IFACE, "GetPosition");
+   e_dbus_message_send(conn, msg, position_cb, -1, NULL);
+   dbus_message_unref(msg);
+
+   cb_position_changed = e_dbus_signal_handler_add(conn, UBUNTU_DBUS_NAME, UBUNTU_OBJECT_PATH, GEOCLUE_POSITION_IFACE, "GetPosition",
+         position_signal_cb, NULL);
+
+   msg2 = dbus_message_new_method_call(UBUNTU_DBUS_NAME, UBUNTU_OBJECT_PATH, GEOCLUE_IFACE, "GetStatus");
+   e_dbus_message_send(conn, msg2, status_cb, -1, NULL);
+   dbus_message_unref(msg2);
+
+   ecore_main_loop_begin();
+
+   e_dbus_shutdown();
+   return ret;
+}
---
+++ PROTO/elocation/src/lib/Elocation.h
@@ -0,0 +1,100 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+
+#include <Ecore.h>
+#include <E_DBus.h>
+
+#define GEOCLUE_DBUS_NAME "org.freedesktop.Geoclue.Master"
+#define GEOCLUE_OBJECT_PATH "/org/freedesktop/Geoclue/Master"
+#define GEOCLUE_POSITION_IFACE "org.freedesktop.Geoclue.Position"
+#define GEOCLUE_ADDRESS_IFACE "org.freedesktop.Geoclue.Address"
+#define GEOCLUE_IFACE "org.freedesktop.Geoclue"
+
+#define GEOCLUE_CLIENT_IFACE "org.freedesktop.Geoclue.MasterClient"
+
+// We should be able to get this informations from the master interface, no?
+#define GSMLOC_DBUS_NAME "org.freedesktop.Geoclue.Providers.Gsmloc"
+#define GSMLOC_OBJECT_PATH "/org/freedesktop/Geoclue/Providers/Gsmloc"
+#define HOSTIP_DBUS_NAME "org.freedesktop.Geoclue.Providers.Hostip"
+#define HOSTIP_OBJECT_PATH "/org/freedesktop/Geoclue/Providers/Hostip"
+#define SKYHOOK_DBUS_NAME "org.freedesktop.Geoclue.Providers.Skyhook"
+#define SKYHOOK_OBJECT_PATH "/org/freedesktop/Geoclue/Providers/Skyhook"
+#define UBUNTU_DBUS_NAME "org.freedesktop.Geoclue.Providers.UbuntuGeoIP"
+#define UBUNTU_OBJECT_PATH "/org/freedesktop/Geoclue/Providers/UbuntuGeoIP"
+
+#define GEOCLUE_ADDRESS_KEY_AREA "area"
+#define GEOCLUE_ADDRESS_KEY_COUNTRY "country"
+#define GEOCLUE_ADDRESS_KEY_COUNTRYCODE "countrycode"
+#define GEOCLUE_ADDRESS_KEY_LOCALITY "locality"
+#define GEOCLUE_ADDRESS_KEY_POSTALCODE "postalcode"
+#define GEOCLUE_ADDRESS_KEY_REGION "region"
+#define GEOCLUE_ADDRESS_KEY_STREET "street"
+
+typedef struct _gc_accuracy
+{
+   int level;
+   double horizontal;
+   double vertical;
+} gc_accuracy;
+
+typedef struct _gc_provider
+{
+   char *name;
+   char *description;
+   char *service;
+   char *path;
+   gc_accuracy accuracy;
+   int status; //Does this make sense?
+
+} gc_provider;
+
+typedef enum {
+   GEOCLUE_POSITION_FIELDS_NONE = 0,
+   GEOCLUE_POSITION_FIELDS_LATITUDE = 1 << 0,
+   GEOCLUE_POSITION_FIELDS_LONGITUDE = 1 << 1,
+   GEOCLUE_POSITION_FIELDS_ALTITUDE = 1 << 2
+} GeocluePositionFields;
+
+typedef enum {
+   GEOCLUE_CONNECTIVITY_UNKNOWN,
+   GEOCLUE_CONNECTIVITY_OFFLINE,
+   GEOCLUE_CONNECTIVITY_ACQUIRING,
+   GEOCLUE_CONNECTIVITY_ONLINE,
+} GeoclueNetworkStatus;
+
+typedef enum {
+   GEOCLUE_ACCURACY_LEVEL_NONE = 0,
+   GEOCLUE_ACCURACY_LEVEL_COUNTRY,
+   GEOCLUE_ACCURACY_LEVEL_REGION,
+   GEOCLUE_ACCURACY_LEVEL_LOCALITY,
+   GEOCLUE_ACCURACY_LEVEL_POSTALCODE,
+   GEOCLUE_ACCURACY_LEVEL_STREET,
+   GEOCLUE_ACCURACY_LEVEL_DETAILED,
+} GeoclueAccuracyLevel;
+
+typedef enum {
+   GEOCLUE_RESOURCE_NONE = 0,
+   GEOCLUE_RESOURCE_NETWORK = 1 << 0,
+   GEOCLUE_RESOURCE_CELL = 1 << 1,
+   GEOCLUE_RESOURCE_GPS = 1 << 2,
+
+   GEOCLUE_RESOURCE_ALL = (1 << 10) - 1
+} GeoclueResourceFlags;
+
+typedef enum {
+   GEOCLUE_STATUS_ERROR,
+   GEOCLUE_STATUS_UNAVAILABLE,
+   GEOCLUE_STATUS_ACQUIRING,
+   GEOCLUE_STATUS_AVAILABLE
+} GeoclueStatus;
+
+typedef enum {
+   GEOCLUE_VELOCITY_FIELDS_NONE = 0,
+   GEOCLUE_VELOCITY_FIELDS_SPEED = 1 << 0,
+   GEOCLUE_VELOCITY_FIELDS_DIRECTION = 1 << 1,
+   GEOCLUE_VELOCITY_FIELDS_CLIMB = 1 << 2
+} GeoclueVelocityFields;
+
