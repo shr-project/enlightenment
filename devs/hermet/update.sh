35dc06c40a99c72c6439b614aa0ca7795fe7fd63
hermet <hermet@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-09-28 15:38:03 +0000

    dev/hermet - updated build script
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@77208 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/hermet/update.sh
+++ devs/hermet/update.sh
@@ -6,7 +6,7 @@ svn update
 
 CFLAGS="-02 -g -W -Wall -Wextra -Wundef -Wshadow"
 
-for e in eina eet evas_generic_loaders evas ecore eeze efreet embryo edje e_dbus eio emotion ethumb elementary e expedite clouseau terminology ephoto
+for e in eina eet evas_generic_loaders evas ecore eeze efreet embryo eio edje e_dbus emotion ethumb elementary e expedite clouseau terminology ephoto
 do 
 	if [ -e $e ] && [ -d $e ]
 	then 
@@ -16,7 +16,6 @@ do
 		make clean
 		make distclean
 		./autogen.sh
-#		CFLAGS=${CFLAGS} make -j 4 ${BUILD_LOG} && sudo make install ${BUILD_LOG} -a && sudo ldconfig
 		CFLAGS=${CFLAGS} make -j 4 2>&1 | tee ../build_"$e".log && sudo make install 2>&1 | tee ../build_"$e".log -a && sudo ldconfig
 		cd ..
 		echo End Done
