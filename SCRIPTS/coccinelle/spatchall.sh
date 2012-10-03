00b22890862d6b9e648aedea589987ddf57456be
stefan <stefan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-10-03 08:45:32 +0000

    SCRIPTS: Update coccinelle script to match latest repo layout.
    Elementary moved, e_dbus connman got renamed, elitaire was moved to BROKEN and
    evas does no longer need librsvg.
    
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@77351 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- SCRIPTS/coccinelle/spatchall.sh
+++ SCRIPTS/coccinelle/spatchall.sh
@@ -71,7 +71,7 @@ function call_spatch {
 if [ ! -z "$TARGET" ]; then
 	TARGETS=$TARGET
 else
-	TARGETS="eina eet embryo evas ecore efreet edje e_dbus eeze e TMP/st/elementary E16 E-MODULES-EXTRA BINDINGS editje edje_viewer eio elitaire elmdentica emotion empower emprint enki enlil ensure ephoto eterm ethumb eve exalt EXAMPLES expedite exquisite imlib2 imlib2_loaders MISC $(ls PROTO | grep -v emote | while read d; do echo PROTO/$a; done) rage TEST"
+	TARGETS="eina eet embryo evas ecore efreet edje e_dbus eeze e elementary E16 E-MODULES-EXTRA BINDINGS editje edje_viewer eio elmdentica emotion empower emprint enki enlil ensure ephoto eterm ethumb eve exalt EXAMPLES expedite exquisite imlib2 imlib2_loaders MISC $(ls PROTO | grep -v emote | while read d; do echo PROTO/$a; done) rage TEST"
 fi
 
 
@@ -98,7 +98,7 @@ for d in $TARGETS; do
 	#  config.h, stdarg.h, stddef.h, Evil.h
 		;;
 	evas)
-		call_spatch $d -I /usr/include/valgrind/ -I /usr/include/librsvg-2 -I ${TOPDIR}/eet/src/lib/ -I src/lib/ -I src/modules/engines/buffer/ $(for i in ${TOPDIR}/evas/src/lib/{include,file,cserve}; do echo -I $i; done)
+		call_spatch $d -I /usr/include/valgrind/ -I ${TOPDIR}/eet/src/lib/ -I src/lib/ -I src/modules/engines/buffer/ $(for i in ${TOPDIR}/evas/src/lib/{include,file,cserve}; do echo -I $i; done)
 	# missing headers:
 	#  windows.h, config.h, Evil.h, altivec.h, Edb.h,
 	#  pixman.h, GL/wglew.h, ApplicationServices/ApplicationServices.h
@@ -125,7 +125,7 @@ for d in $TARGETS; do
 	#  config.h, stddef.h
 		;;
 	e_dbus)
-		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) \
+		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman0_7x,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) \
 	# missing headers:
 	#  config.h, stddef.h
 		;;
@@ -135,7 +135,7 @@ for d in $TARGETS; do
 	#  config.h
 		;;
 	e)
-		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib -I ${TOPDIR}/efreet/src/lib -I src/bin/ -I src/modules/illume2/
+		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman0_7x,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib -I ${TOPDIR}/efreet/src/lib -I src/bin/ -I src/modules/illume2/
 	# missing headers:
 	#  config.h, stddef.h, stdarg
 	#  machine/apm_bios.h, CFBase.h, CFNumber.h, CFArray.h, CFDictionary.h, CFRunLoop.h, ps/IOPSKeys.h, ps/IOPowerSources.h
@@ -143,18 +143,18 @@ for d in $TARGETS; do
 
 		;;
 	TMP/st/elementary)
-		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib -I ${TOPDIR}/efreet/src/lib -I ${TOPDIR}/PROTO/libeweather/src/lib/ -I ./ -I src/lib/ 
+		call_spatch $d -I ${TOPDIR}/eina/src/include/ $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman0_7x,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib -I ${TOPDIR}/efreet/src/lib -I ${TOPDIR}/PROTO/libeweather/src/lib/ -I ./ -I src/lib/ 
 		;;
 	E16)
 		call_spatch $d -I /usr/include/gtk-2.0/ -I /usr/include/glib-2.0/ -I /usr/include/pango-1.0/ -I /usr/include/libgtop-2.0/ -I /usr/include/dbus-1.0/ -I epplets/api/ -I epplets/epplets/ -I e/src/
 		;;
 	E-MODULES-EXTRA)
 		# !@##$&#@$##&% -- don't bother about includes, let's hope it works
-		call_spatch $d $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/e/src/bin/
+		call_spatch $d $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman0_7x,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/e/src/bin/
 		;;
 	*)
 		# !@##$&#@$##&% -- don't bother about includes, let's hope it works
-		call_spatch $d $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eina/src/include/ -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib
+		call_spatch $d $(for i in ${TOPDIR}/ecore/src/lib/ecore*; do echo -I $i; done) $(for i in ${TOPDIR}/e_dbus/src/lib/{bluez,connman0_7x,dbus,hal,notification,ofono,ukit}; do echo -I $i; done) -I ${TOPDIR}/eina/src/include/ -I ${TOPDIR}/eet/src/lib/ -I ${TOPDIR}/evas/src/lib/ -I ${TOPDIR}/edje/src/lib
 		;;
 	esac
 done
