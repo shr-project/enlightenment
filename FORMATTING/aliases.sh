2a05c9372809146a85457556bab40b41bcc76ff9
discomfitor <discomfitor@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-10-10 11:56:32 +0000

    add FOREACH_SAFE macros to shell format fixing functions
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@77754 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- FORMATTING/aliases.sh
+++ FORMATTING/aliases.sh
@@ -1,8 +1,8 @@
 cformat() {
 	ecrustify -c svn/trunk/FORMATTING/ecrustify.cfg --replace $@
-	sed -i 's/FOREACH (/FOREACH(/g;s/FREE (/FREE(/g' $@
+	sed -i 's/FOREACH (/FOREACH(/g;s/FREE (/FREE(/g;s/FOREACH_SAFE (/FOREACH_SAFE(/g' $@
 }
 hformat() {
 	ecrustify -c svn/trunk/FORMATTING/ecrustify-headers.cfg --replace $@
-	sed -i 's/FOREACH (/FOREACH(/g;s/FREE (/FREE(/g' $@
+	sed -i 's/FOREACH (/FOREACH(/g;s/FREE (/FREE(/g;s/FOREACH_SAFE (/FOREACH_SAFE(/g' $@
 }
