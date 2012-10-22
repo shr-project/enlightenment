498cb9483d06cf06b482e463f6350a8e781109f8
stefan <stefan@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-10-22 12:59:29 +0000

    PROTO/elocation: Add some test code for the location converter.
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@78334 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/elocation/src/bin/elocation-test.c
+++ PROTO/elocation/src/bin/elocation-test.c
@@ -17,6 +17,42 @@ status_changed(void *data, int ev_type, void *event)
 }
 
 static Eina_Bool
+rgeocode_arrived(void *data, int ev_type, void *event)
+{
+   Elocation_Address *address;
+
+   address = event;
+   printf("Geocode reply:\n");
+   printf("Country: %s\n", address->country);
+   printf("Countrycode: %s\n", address->countrycode);
+   printf("Locality: %s\n", address->locality);
+   printf("Postalcode: %s\n", address->postalcode);
+   printf("Region: %s\n", address->region);
+   printf("Timezone: %s\n", address->timezone);
+   printf("Accuracy level: %i\n", address->accur->level);
+   printf("Accuracy horizontal: %f\n", address->accur->horizontal);
+   printf("Accuracy vertical: %f\n", address->accur->vertical);
+
+   return ECORE_CALLBACK_DONE;
+}
+
+static Eina_Bool
+geocode_arrived(void *data, int ev_type, void *event)
+{
+   Elocation_Position *position;
+
+   position = event;
+   printf("Reverse geocode reply:\n");
+   printf("Latitude:\t %f\n", position->latitude);
+   printf("Longitude:\t %f\n", position->longitude);
+   printf("Altitude:\t %f\n", position->altitude);
+   printf("Accuracy level: %i\n", position->accur->level);
+   printf("Accuracy horizontal: %f\n", position->accur->horizontal);
+   printf("Accuracy vertical: %f\n", position->accur->vertical);
+   return ECORE_CALLBACK_DONE;
+}
+
+static Eina_Bool
 address_changed(void *data, int ev_type, void *event)
 {
    Elocation_Address *address;
@@ -55,8 +91,8 @@ position_changed(void *data, int ev_type, void *event)
 int
 main()
 {
-   Elocation_Address *address;
-   Elocation_Position *position;
+   Elocation_Address *address, *addr_geocode;
+   Elocation_Position *position, *pos_geocode;
    int status;
 
    ecore_init();
@@ -70,13 +106,31 @@ main()
    ecore_event_handler_add(ELOCATION_EVENT_STATUS, status_changed, NULL);
    ecore_event_handler_add(ELOCATION_EVENT_POSITION, position_changed, NULL);
    ecore_event_handler_add(ELOCATION_EVENT_ADDRESS, address_changed, NULL);
+   ecore_event_handler_add(ELOCATION_EVENT_GEOCODE, geocode_arrived, NULL);
+   ecore_event_handler_add(ELOCATION_EVENT_REVERSEGEOCODE, rgeocode_arrived, NULL);
 
    elocation_status_get(&status);
    elocation_position_get(position);
    elocation_address_get(address);
 
+   addr_geocode = elocation_address_new();
+   pos_geocode = elocation_position_new();
+
+   elocation_freeform_address_to_position("London", pos_geocode);
+
+   pos_geocode->latitude = 51.7522;
+   pos_geocode->longitude = -1.25596;
+   pos_geocode->accur->level = 3;
+   elocation_position_to_address(pos_geocode, addr_geocode);
+
+   addr_geocode->locality = "Cambridge";
+   addr_geocode->countrycode = "UK";
+   elocation_address_to_position(addr_geocode, pos_geocode);
+
    ecore_main_loop_begin();
 
+   elocation_address_free(addr_geocode);
+   elocation_position_free(pos_geocode);
    elocation_address_free(address);
    elocation_position_free(position);
    elocation_shutdown();
