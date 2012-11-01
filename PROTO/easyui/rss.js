f6d2781f918b6aae1ec4fa3700f2e646a183d217
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-11-01 13:20:42 +0000

    EasyUI: Document RSS application using JS Duck
    Patch by: Joao Paulo Fernandes Ventura <ventura@profusion.mobi>
    
    
    
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@78778 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/easyui/rss.js
+++ PROTO/easyui/rss.js
@@ -3,18 +3,32 @@ ajax = require('ajax');
 
 const URL = 'http://feeds.bbci.co.uk/news/uk/rss.xml';
 
+/** @extends EUI.WebController */
 WebBrowser = EUI.WebController({
+  /**
+   * @param {String} url
+   * @event
+   */
   init: function(url) {
     this.url = url;
   }
 })
 
+/** @extends EUI.Model */
 RSSModel = EUI.Model({
+  /**
+   * @param {String} url
+   * @param {String} path
+   * @event
+   */
   init: function(url, path) {
     this.url = this.url || url;
     this._array = [];
     this.refresh();
   },
+  /**
+   * Refresh model after finishing XML file download.
+   */
   refresh: function() {
     ajax.ajax(this.url, {
       onSuccess: function(request) {
@@ -26,9 +40,11 @@ RSSModel = EUI.Model({
       }.bind(this)
     });
   },
+  /** @inheritdoc */
   length: function() {
     return this._array.length;
   },
+  /** @inheritdoc */
   itemAtIndex: function(index) {
     var item = this._array[index];
 
@@ -55,12 +71,20 @@ RSSModel = EUI.Model({
   }
 });
 
+/** @extends EUI.ListController */
 RSSList = EUI.ListController({
+  /** @type {String} */
   style: 'double_label',
+  /**
+   * @param {String} url
+   * @param {String} title
+   * @event
+   */
   init: function(url, title) {
     this.title = title;
     this.model = new RSSModel(url);
   },
+  /** @inheritdoc */
   itemAtIndex: function(index) {
     var item = this.model.itemAtIndex(index);
 
@@ -70,6 +94,7 @@ RSSList = EUI.ListController({
       icon: item.icon
     };
   },
+  /** @inheritdoc */
   selectedItemAtIndex: function(index) {
     var item = this.model.itemAtIndex(index);
     this.pushController(new WebBrowser(item.link));
