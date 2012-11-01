572194a7cdbd97822559b2620ce123543a511ef1
acidx <acidx@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-11-01 13:19:33 +0000

    EasyUI: Document Browser application using JS Duck
    Summary: Document Browser application using JS Duck.
    
    
    Patch by: Joao Paulo Fernandes Ventura <ventura@profusion.mobi>
    
    
    
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@78775 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- PROTO/easyui/browser.js
+++ PROTO/easyui/browser.js
@@ -1,8 +1,15 @@
 var EUI = require('eui');
 
+/** @extends EUI.WebController */
 WebPageController = EUI.WebController({
+  /** @type {String} */
   url: 'http://www.enlightenment.org',
+  /** @type {Boolean} */
   titleVisible: false,
+  /**
+   * @param {String} title
+   * @event
+   */
   didChangeTitle: function(title) {
     var index = webPagesModel.indexOf(this);
     webPagesModel.updateItemAtIndex(index, {title: title});
@@ -11,9 +18,13 @@ WebPageController = EUI.WebController({
 
 webPagesModel = new EUI.ArrayModel([new WebPageController()]);
 
+/** @extends EUI.TabController */
 BrowserController = EUI.TabController({
+  /** @type {EUI.ArrayModel} */
   model: webPagesModel,
+  /** @type {Array} */
   toolbarItems: [{tag: 'new', label: '+'}],
+  /** @inheritdoc */
   selectedToolbarItem: function(item) {
     switch (item.tag) {
     case 'new':
