55c737eb1b59dbe4e4750d3765aa518fbe3d3a19
seoz <seoz@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-11-09 03:54:47 +0000

    devs/seoz build.sh: Updated build script.
    1. Added manual build instructions.
    2. Use evas in trunk/efl. Now it's stable with OpenGL.
    3. Print errors to file and stdout.
    
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@79007 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/seoz/build.sh
+++ devs/seoz/build.sh
@@ -4,13 +4,15 @@
 # trunk$ ./devs/seoz/build.sh
 ##########################################
 
-###### Ubuntu 12.04 Pre-requisites #######
+###### Ubuntu >= 12.04 Pre-requisites #######
 # sudo apt-get install build-essential automake libtool ccache zlib1g-dev libjpeg-dev libfreetype6-dev libdbus-1-dev liblua5.1-0-dev g++ libxext-dev libxrender-dev libpng12-dev libxrandr-dev libfontconfig1-dev autopoint libxcomposite-dev libxcursor-dev libxdamage-dev libxdmcp-dev libxfixes-dev libxfont-dev  libxi-dev libxinerama-dev libxss-dev libxv-dev libtiff4-dev librsvg2-dev libfribidi-dev libcurl4-openssl-dev libexif-dev libiptcdata0-dev libxml2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libudev-dev libxcb-shape0-dev libsigc++-2.0-dev byacc libsqlite3-dev python-dev imagemagick libpam0g-dev python-pyparsing cython libcanberra-dev libv8-dev iptables-dev doxygen
 ##########################################
 
 ############## Manual Build ##############
 # for enjoy : lightmediascanner http://lms.garage.maemo.org/
+#             ./autogen.sh && make && sudo make install && sudo ldconfig
 # for ephysics : Bullet Physics Engine http://bulletphysics.org/wordpress/
+#                Build instruction (Korean) : http://seoz.egloos.com/3857085
 # for e17 connman module & econnman : http://www.kernel.org/pub/linux/network/connman/connman-1.6.tar.gz
 ##########################################
 
@@ -25,8 +27,7 @@ alias make='make -j6'
 export BUILD_E_OPTION="eeze ephysics"
 export BUILD_ETHUMB_OPTION="PROTO/epdf"
 export BUILD_ELM_OPTION="PROTO/emap"
-export BUILD_BASIC1="efl" #eina eet eo embryo
-export BUILD_BASIC2="ecore eio edje edbus efreet expedite "$BUILD_ETHUMB_OPTION" emotion ethumb "$BUILD_ELM_OPTION" elementary "$BUILD_E_OPTION" e"
+export BUILD_CORE_EFL="ecore eio edje edbus efreet expedite "$BUILD_ETHUMB_OPTION" emotion ethumb "$BUILD_ELM_OPTION" elementary "$BUILD_E_OPTION" e"
 export BUILD_PYTHON_BINDINGS="BINDINGS/python/python-evas BINDINGS/python/python-ecore BINDINGS/python/python-elementary BINDINGS/python/python-edje BINDINGS/python/python-emotion BINDINGS/python/python-e_dbus"
 export BUILD_CPP_BINDINGS="BINDINGS/cxx/eflxx BINDINGS/cxx/einaxx BINDINGS/cxx/evasxx BINDINGS/cxx/ecorexx BINDINGS/cxx/edjexx BINDINGS/cxx/elementaryxx"
 export BUILD_BINDINGS=$BUILD_PYTHON_BINDINGS" "$BUILD_CPP_BINDINGS" "
@@ -39,6 +40,7 @@ export BUILD_WITH_CMAKE="ecrire"
 
 PWD=`pwd`
 LOG_WARN_FILE=$PWD"/warnings.txt"
+LOG_WARN_TMP_FILE=$PWD"/warnings_tmp.txt"
 
 function create_log()
 {
@@ -46,10 +48,29 @@ function create_log()
 	touch $LOG_WARN_FILE
 }
 
+function build_efl()
+{
+	echo " "
+	echo "============ EFL ============"
+	pushd efl
+		echo "" >> $LOG_WARN_FILE
+		echo "[EFL]" >> $LOG_WARN_FILE
+		if [ -f Makefile ]; then
+			make clean distclean || true
+		fi
+		./autogen.sh --disable-doc --disable-cpu-sse3
+		make 3>&1 1>&2 2>&3 | tee $LOG_WARN_TMP_FILE
+		cat $LOG_WARN_TMP_FILE >> $LOG_WARN_FILE
+		rm $LOG_WARN_TMP_FILE
+		sudo make install
+		sudo ldconfig
+	popd
+}
+
 function build()
 {
 	build_dir=$1
-	autogen_option="$2"
+	autogen_option=$2
 	for I in $build_dir; do
   	pushd $I
 		echo " "
@@ -60,11 +81,11 @@ function build()
 			make clean distclean || true
 		fi
 		./autogen.sh "$autogen_option"
-		make 2>> $LOG_WARN_FILE 
+		make 3>&1 1>&2 2>&3 | tee $LOG_WARN_TMP_FILE
+		cat $LOG_WARN_TMP_FILE >> $LOG_WARN_FILE
+		rm $LOG_WARN_TMP_FILE
 		sudo make install
 		sudo ldconfig
-
-		#ctags -h ".h.x.cpp.c" -R
   	popd
 	done
 }
@@ -183,9 +204,8 @@ function e17_restart()
 
 create_log
 
-build "$BUILD_BASIC1" --disable-doc
-build evas --disable-cpu-sse3 --disable-doc
-build "$BUILD_BASIC2" --disable-doc
+build_efl # eina eet eo embryo evas
+build "$BUILD_CORE_EFL" --disable-doc
 build "$BUILD_PYTHON_BINDINGS" "--prefix=/usr/local"
 #build "$BUILD_CPP_BINDINGS"
 build "$BUILD_E_MODULES $BUILD_ETC $BUILD_EXAMPLE "
