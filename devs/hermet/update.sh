a341e03f6ed7195478d27a9fa26417032d3a473d
hermet <hermet@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-11-26 07:27:20 +0000

    devs/hermet - updated
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@79673 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/hermet/update.sh
+++ devs/hermet/update.sh
@@ -4,8 +4,6 @@ echo Updating EFL Packages
 
 svn update
 
-CFLAGS="-O0 -g -Wall -j 8"
-
 for e in efl evas_generic_loaders evas ecore eeze efreet edje e_dbus eio emotion ethumb elementary e expedite clouseau terminology ephoto
 do 
 	if [ -e $e ] && [ -d $e ]
@@ -15,8 +13,8 @@ do
 		cd $e
 		make clean
 		make distclean
-		CFLAGS=${CFLGAS} ./autogen.sh
-		make 2>&1 | tee ../build_"$e".log && sudo make install && sudo ldconfig 2>&1 | tee ../build_"$e".log -a
+		./autogen.sh
+		make -j8 2>&1 | tee ../build_"$e".log && sudo make install && sudo ldconfig 2>&1 | tee ../build_"$e".log -a
 		cd ..
 		echo End Done
 	fi 
