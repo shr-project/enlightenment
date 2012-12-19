1ad553e6734a17d956a57b3e75ba08bc9463da8c
mike_m <mike_m@7cbeb6ba-43b4-40fd-8cce-4c39aea84d33>
2012-12-19 10:58:26 +0000

    Add libtiff4-dev to dependency list
    git-svn-id: svn+ssh://svn.enlightenment.org/var/svn/e/trunk@81345 7cbeb6ba-43b4-40fd-8cce-4c39aea84d33

--- devs/mike_m/build.sh
+++ devs/mike_m/build.sh
@@ -11,7 +11,7 @@ deplist="autotools-dev automake autopoint libtool zlib1g-dev
 	librsvg2-dev libfribidi-dev libpixman-1-dev
 	libxss-dev libxp-dev
 	libxtst-dev graphviz libasound2-dev libpam0g-dev libdbus-1-dev
-	libxcb-keysyms1-dev"
+	libxcb-keysyms1-dev libtiff4-dev"
 
 defpkgs="efl eio eeze edje e_dbus edbus efreet PROTO/libeweather elementary e emotion terminology"
 #
